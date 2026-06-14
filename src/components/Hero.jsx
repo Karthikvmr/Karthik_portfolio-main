04091d7f2e01383980d48b4be1855ef003b5dba7
Karthikvmr <karthickssp2@gmail.com>
2026-06-14 12:03:11 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
             data-aos="fade-up"
             className="text-black text-3xl md:text-5xl font-bold mb-4 tracking-tight"
           >
-            Hi, I’m a <br /> <span className="text-White text-3xl md:text-5xl font-bold mb-4 tracking-tight">React Developer</span>
+            Hi, I’m a <br /> <span className="text-white text-3xl md:text-5xl font-bold mb-4 tracking-tight">React Developer</span>
           </h1>
 
           {/* Subheading */}
